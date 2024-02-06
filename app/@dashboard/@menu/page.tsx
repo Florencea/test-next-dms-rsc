bc7d31161d578828e11e47971f35fcff823b60ae
Florencea <bearflorencea@gmail.com>
2024-02-06 23:04:09 +0800

    fix: err

--- app/@dashboard/@menu/page.tsx
+++ app/@dashboard/@menu/page.tsx
@@ -1,5 +1,6 @@
+import { DEFAULT_PRIVATE_ROUTE } from "@/constants/constants";
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/user");
+  redirect(DEFAULT_PRIVATE_ROUTE);
 }
